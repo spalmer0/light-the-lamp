7087961790eb75c711aab07731a41deeb01e9ca9
Stuart Palmer <palmersf@gmail.com>
2021-01-06 21:24:07 -0500

    configs route props

--- project-3-client/src/App.js
+++ project-3-client/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+
+
+
+// 5. import Switch and Route from React Router and then define the various routes for all the pages
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+        <main>
+          <Switch>
+            <Route exact path="/" render={props =>
+              <Home />
+            } />
+            <Route exact path="/dashboard" render={props =>
+              <Dashboard />
+            }/>
+            <Route exact path="/signup" render={props =>
+              <Signup {...props} />
+            }/>
+            <Route exact path="/login" render={props =>
+              <Login {...props} />
+            }/>
+            
+            
+          </Switch>
+          
+        </main>
+      <Footer />
     </div>
   );
 }
