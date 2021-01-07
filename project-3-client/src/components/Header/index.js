0350132b0686508e8493521a4f74cd13587a4673
Stuart Palmer <palmersf@gmail.com>
2021-01-06 21:24:27 -0500

    adds nav links and styles

--- project-3-client/src/components/Header/index.js
+++ project-3-client/src/components/Header/index.js
@@ -1,15 +1,26 @@
+import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
 
 function Header(props) {
     return (
         <header className={styles.Header}>
-            <h1>Project 3</h1>
+            <Link to="/">
+                <h1>Project 3</h1>
+            </Link>
             <nav>
                 <ul>
-                    <li>Login</li>
-                    <li>Logout</li>
-                    <li>Signup</li>
-                    <li>Dashboard</li>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                    <li>
+                        <Link to="">Logout</Link>
+                    </li>
+                    <li>
+                        <Link to="/signup">Signup</Link>
+                    </li>
+                    <li>
+                        <Link to="/dashboard">Dashboard</Link>
+                    </li>
                 </ul>
             </nav>
         </header>
